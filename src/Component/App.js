748917c8f9c411ac05c4dd59174cbc8b8896b94e
ayahdweikat <dweikatayah99@gmail.com>
2022-10-19 20:49:37 +0300

    reset,add,delete-done

--- src/Component/App.js
+++ src/Component/App.js
@@ -5,11 +5,66 @@ class App extends Component {
   state={
     list:[{title:'ayah', description:'web-developer'},
     {title:'ali', description:'building-engineer'},
-    {title:'sfgfgh', description:'asddsdfftgyuiop'}
-  ]}
-  editList=(currentlist)=>{
-    console.log("edit", currentlist);
+    {title:'sfgfgh', description:'asddsdfftgyuiop'},],
+    title:'',
+    description:'',
+    _currentlist:{
+      title:'',
+      description:''
+    }
   }
+  onTitleChange(e){
+    let value=e.target.value;
+    this.setState({
+      title:value
+    })
+  }
+  onDescChange(e){
+    let value=e.target.value;
+    this.setState({
+      description:value
+    })
+  }
+  addToList(e){
+    e.preventDefault();
+    let _list = this.state.list;
+    let obj={
+      title:'',
+      description:''
+    }
+    obj.title= this.state.title;
+    obj.description= this.state.description;
+    _list.push(obj)
+    this.setState({
+      list: _list
+    })
+    e.target.reset();
+  }
+  defaultInput(currentlist){
+    let __list = this.state._currentlist;
+    __list.title= currentlist.title;
+    __list.description= currentlist.description;
+    // console.log(__list.title, __list.description);
+    this.setState({
+    _currentlist:__list
+    })
+  }
+  editList=(currentlist,e)=>{
+    this.defaultInput(currentlist);
+    let _list=this.state.list;
+    let idx = _list.indexOf(currentlist);
+    let obj={
+      title:'',
+      description:''
+    }
+    obj.title= this.state.title;
+    obj.description= this.state.description;
+    _list[idx]=obj;
+    this.setState({
+      list: _list
+    })
+  }
+  
   deleteList=(currentlist)=>{
     let _list=this.state.list;
     let idx = _list.indexOf(currentlist);
@@ -20,16 +75,19 @@ class App extends Component {
   }
   render() {
     return (
-      <div className='todo'>
+      <div>
         <h2 className='App'>Simple To Do List</h2>
+        <form className='todo' onSubmit={this.addToList.bind(this)}> 
           <label htmlFor="title">Title</label>
-          <input type="text" id='title' />
+          <input type="text" name='title' value={this.state.list.title} id='title' onChange={this.onTitleChange.bind(this)} defaultValue={this.state._currentlist.title}/>
           <label htmlFor="desc">Description</label>
-          <textarea name="desc" id="desc" cols="10" rows="5"></textarea>
+          <textarea name="desc" value={this.state.list.description} id="desc" cols="10" rows="5" onChange={this.onDescChange.bind(this)} defaultValue={this.state._currentlist.description}></textarea>
           <div>
-            <button>Add</button>
-            <button>Reset</button>
+            <button type="submit">Add</button>
+            <button type='reset'>Reset</button>
           </div>
+        </form>
+          
         
 
         <hr />
