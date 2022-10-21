bcaa5bbaf6b0b18f2ff77899f619aedd0c0cc779
ayahdweikat <dweikatayah99@gmail.com>
2022-10-21 19:26:03 +0300

    Done-React

--- src/Component/App.js
+++ src/Component/App.js
@@ -4,14 +4,17 @@ import List from './List';
 class App extends Component {
   state={
     list:[{title:'ayah', description:'web-developer'},
-    {title:'ali', description:'building-engineer'},
-    {title:'sfgfgh', description:'asddsdfftgyuiop'},],
+    {title:'ali', description:'building-engineer'},],
     title:'',
     description:'',
     _currentlist:{
       title:'',
       description:''
-    }
+    },
+    btn1: "add",
+    btn2:"reset",
+    index:-1,
+    current:{}
   }
   onTitleChange(e){
     let value=e.target.value;
@@ -25,6 +28,17 @@ class App extends Component {
       description:value
     })
   }
+  reset(){
+    let obj={
+      title:"",
+      description:""
+    }
+    this.setState({
+      title:"",
+      description:"",
+      _currentlist:obj
+    })
+  }
   addToList(e){
     e.preventDefault();
     let _list = this.state.list;
@@ -34,7 +48,7 @@ class App extends Component {
     }
     obj.title= this.state.title;
     obj.description= this.state.description;
-    _list.push(obj)
+    let x = (obj.title !== "" && obj.description !=="" )? _list.push(obj): null
     this.setState({
       list: _list
     })
@@ -44,27 +58,51 @@ class App extends Component {
     let __list = this.state._currentlist;
     __list.title= currentlist.title;
     __list.description= currentlist.description;
-    // console.log(__list.title, __list.description);
     this.setState({
     _currentlist:__list
     })
   }
   editList=(currentlist,e)=>{
+    e.preventDefault();
     this.defaultInput(currentlist);
-    let _list=this.state.list;
-    let idx = _list.indexOf(currentlist);
-    let obj={
-      title:'',
-      description:''
-    }
-    obj.title= this.state.title;
-    obj.description= this.state.description;
-    _list[idx]=obj;
+    let idx = this.state.list.indexOf(currentlist);
     this.setState({
-      list: _list
+      btn1:"update",
+      btn2:"cancel",
+      index:idx,
+      current: currentlist
     })
   }
-  
+  update=(e)=>{
+    e.preventDefault();
+    let idx = this.state.index;
+    let _list = this.state.list;
+    _list[idx].title = this.state.title;
+    _list[idx].description = this.state.description;
+    console.log(this.state.index, this.state.title, this.state.description);
+    
+    this.setState({
+      list:_list,
+      btn1:"add",
+      btn2:"reset",
+    })
+    
+    
+  }
+  cancel(){
+    let obj={
+      title:"",
+      description:""
+    }
+    this.setState({
+      title:"",
+      description:"",
+      _currentlist:obj,
+      btn1:"add",
+      btn2:"reset"
+    })
+
+  }
   deleteList=(currentlist)=>{
     let _list=this.state.list;
     let idx = _list.indexOf(currentlist);
@@ -77,14 +115,15 @@ class App extends Component {
     return (
       <div>
         <h2 className='App'>Simple To Do List</h2>
-        <form className='todo' onSubmit={this.addToList.bind(this)}> 
+        <form className='todo' onSubmit={(this.state.btn1 ==="add")? this.addToList.bind(this): this.update.bind(this)}
+         > 
           <label htmlFor="title">Title</label>
-          <input type="text" name='title' value={this.state.list.title} id='title' onChange={this.onTitleChange.bind(this)} defaultValue={this.state._currentlist.title}/>
+          <input type="text" name='title' id='title'  onChange={this.onTitleChange.bind(this)} defaultValue={this.state._currentlist.title}/>
           <label htmlFor="desc">Description</label>
-          <textarea name="desc" value={this.state.list.description} id="desc" cols="10" rows="5" onChange={this.onDescChange.bind(this)} defaultValue={this.state._currentlist.description}></textarea>
+          <textarea type="text" name="desc" id="desc" cols="10" rows="5" onChange={this.onDescChange.bind(this)} defaultValue={this.state._currentlist.description}></textarea>
           <div>
-            <button type="submit">Add</button>
-            <button type='reset'>Reset</button>
+            <button type="submit">{this.state.btn1}</button>
+            <button type="reset" onClick={(this.state.btn2 ==="reset")? this.reset.bind(this): this.cancel.bind(this)}>{this.state.btn2}</button>
           </div>
         </form>
           
